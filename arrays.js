09dbd64179b613842b5aa41f0615e1753902525c
Elenfrancyne <elenfrancyne@gmail.com>
2022-04-19 13:20:27 -0300

    ATUALIZANDO ARRAY

--- arrays.js
+++ arrays.js
@@ -1,6 +1,13 @@
-//arrays- Lista deobjetos, ou elementos
-let nome = { "Elen" }
+//arrays- Lista de objetos, ou elementos
+// iterar a array para achar as coisas, um looping , percorrer o array
+//let nome = { "Elen" }
 
-letalunas = ['Adriana', 'Amitair', 'Ana Elisa', 'Elen'];
+let alunas = ['Adriana', 'Amitair', 'Ana Elisa', 'Elen']
 
-console.log(alunas[4]);
+//console.log(alunas[3])
+//console.log(alunas.length)
+let listaDeValores = new Array();
+//console.log(listaDeValores);
+
+let signos = Array.of('Aquario', 'Libra', 'Gemeos', 'Sagitario'); // outra forma de criar um array sem colchetes
+console.log(signos);
